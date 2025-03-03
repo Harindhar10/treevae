50e7fbfbd2f8391e3d608e5290267a111eaaf0c8
Harindhar10 <harindhar99@gmail.com>
2025-03-03 20:16:58 +0530

    Revert "Update train.py"
    This reverts commit 3461cb134c5f13b7bf48c41524c2e81465859e1a.

--- train/train.py
+++ train/train.py
@@ -52,7 +52,7 @@ def run_experiment(configs):
 	# ADD YOUR WANDB ENTITY
 	wandb.init(
 		project="treevae",
-		entity="be20b012-iit-madras-foundation",
+		entity="iit-madras-foundation",
 		config=configs, 
 		mode=configs['globals']['wandb_logging']
 	)
